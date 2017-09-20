f989cc8806825a7e0ad2acf7b7730e4e1d86908c
rfernandes <rfernandes@cleartech.com.br>
2017-09-20 17:38:18 -0300

    ajustando metodo de exclur cliente para realocar agendamentos para um registro valido e excluir de forma que fique apenas 1 valido, e caso nao seja possivel mostra um alerta na pagina

--- src/main/java/br/com/doutorti/willsalon/model/repositories/IClientRepository.java
+++ src/main/java/br/com/doutorti/willsalon/model/repositories/IClientRepository.java
@@ -10,12 +10,15 @@ import br.com.doutorti.willsalon.model.ClientEntity;
 
 public interface IClientRepository extends JpaRepository<ClientEntity, Long> {
 
-	@Query("select c from ClientEntity c where upper(c.name) like upper(concat('%',?1,'%')) order by c.name asc")
-	public List<ClientEntity> findByNameContaining(String name);
-	
-	@Query("select c1 from ClientEntity c1 where c1.id = (select min(c.id) from ClientEntity c where c.phone like concat('%',?1,'%') and DATE(c.birthDate) = DATE(?2))")
-	public ClientEntity findByPhoneContainingAndBirthDate(String phone, Date birthDate);
+	@Query( "select c from ClientEntity c where upper(c.name) like upper(concat('%',?1,'%')) order by c.name asc" )
+	public List<ClientEntity> findByNameContaining( String name );
+
+	@Query( "select c1 from ClientEntity c1 where c1.id = (select min(c.id) from ClientEntity c where c.phone like concat('%',?1,'%') and DATE(c.birthDate) = DATE(?2))" )
+	public ClientEntity findByPhoneContainingAndBirthDate( String phone, Date birthDate );
 
 	public List<ClientEntity> findAllByOrderByNameAsc();
+
+	@Query( "select min(c.id) from ClientEntity c where c.phone = ?1 and DATE(c.birthDate) = DATE(?2) and c.id != ?3" )
+	public Long findClientWithSamePhoneAndBirthDate( String phone, Date birthDate, Long id );
 
 }
