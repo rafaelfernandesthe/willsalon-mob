8d725eab717f7d1f628c4ad7266f6950a1cbb139
ptome <ptome@cleartech.com.br>
2018-01-25 11:59:17 -0200

    Customizacao do website para o Salao Mais Bonita

--- www/public_html/index.php
+++ www/public_html/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WILL SALON</title>
+    <title>MAIS BONITA</title>
     <!-- Google Font -->
     <link href='css/google_font_01.css' rel='stylesheet' type='text/css'>
 
@@ -43,6 +43,7 @@
 	  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
 	
+	<!--
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -53,8 +54,9 @@
   ga('send', 'pageview');
 
 </script>
-
-<!-- Facebook Pixel Code -->
+-->
+<!--
+<!-- Facebook Pixel Code 
 <script>
 !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
@@ -65,8 +67,7 @@ fbq('init', '245786829216560'); // Insert your pixel ID here.
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=245786829216560&ev=PageView&noscript=1"/></noscript>
-<!-- DO NOT MODIFY -->
-<!-- End Facebook Pixel Code -->
+-->
 
 </head>
 
@@ -112,17 +113,17 @@ fbq('track', 'PageView');
                         </a>
                         <div class="show-only-mobile" style="text-align:center;">
                         
-                        <a class="social_icons-header" target="_blank" href="http://facebook.com/willsalon1"><i class="fa fa-facebook-official" style="font-size:25px;color:#4a90e2;"></i></a>
-                        <a class="social_icons-header" target="_blank" href="http://instagram.com/_u/willsalon/"><i class="fa fa-instagram" style="font-size:25px;color:#fb3958;"></i></a>
+                        <a class="social_icons-header" target="_blank" href="https://www.facebook.com/salaomaisbonita/"><i class="fa fa-facebook-official" style="font-size:25px;color:#4a90e2;"></i></a>
+                        <a class="social_icons-header" target="_blank" href="http://instagram.com/_u/salaomaisbonita/"><i class="fa fa-instagram" style="font-size:25px;color:#fb3958;"></i></a>
                         </div>
                     </div>
                     
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-right">
                             <!-- NAV -->
-                            <li><a href="#HOME"></a> </li>
+                            <li><a id="homeMenu" href="#HOME"></a> </li>
                             <li><a href="#SCHEDULING">AGENDAMENTO</a> </li>
-                            <li><a href="#ABOUT">O WILL SALON</a> </li>
+                            <li><a href="#ABOUT">SOBRE NÓS</a> </li>
                             <li><a href="#EMPLOYEES">PROFISSIONAIS</a> </li>
                             <li><a href="#SERVICES">SERVIÇOS</a> </li>
                             <li><a href="#WORKSHOP">CURSO E WORKSHOP</a> </li>
@@ -166,13 +167,14 @@ fbq('track', 'PageView');
 									 pattern="\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}" placeholder="(00) 0000-00000" 
 									 aria-describedby="basic-addon1" required="true">
 							</div>
+							</br>
 							<span>*Informe a data de nasc. no formato 99/99/9999</span>
 							<div name="birthDate" class="input-group input-group-lg home_text">
 								<span class="input-group-addon" id="phone">Data de Nasc.</span>
 								<input name="birthDate" id="birthDateInput" type="date" class="form-control" placeholder="00/00/0000" 
 								aria-describedby="basic-addon1" required="true" pattern="\d{2}/\d{2}/\d{4}">
 							</div>
-							<div class="input-group col-md-12" style="text-align:center;">
+							<div class="col-md-12" style="text-align:center;">
 								<p><button type="submit" class="btn btn-default">Ver/Continuar Agendamento</button></p>
 								</br><a target="_blank" href="http://104.236.67.194/admin">Acesso Restrito</a>
 					                        <!--</br>Realize seu agendamento. Em breve entraremos em contato com você!-->
@@ -199,7 +201,7 @@ fbq('track', 'PageView');
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="section_title">
-                        <h2>O Will Salon</h2>
+                        <h2>SOBRE NÓS!</h2>
                         <p>
  O WILL SALON, mais que um salão de beleza, é um ambiente aconchegante e descolado, com mais de 10 anos de tradição, temos uma equipe de profissionais dedicados ao que mais amam fazer e prontos para atende-lo de uma forma totalmente personalizada.</p>
 
@@ -398,8 +400,13 @@ Valorização humana</br>
                 <div class="row">
                     <div class="col-md-4">
                         <div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-	                        <div><i class="pe-7s-star"></i></div>	
-                                <h3>CABELO</h3>
+
+								<div>
+								  <h3>CABELO</h3>
+								</div>
+						</div>
+				<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
+
                                 <p>
 				<ul>
 					Acabamento</br>
@@ -428,9 +435,11 @@ Valorização humana</br>
                     </div>
 		<div class="col-md-4">
                         <div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-	                        <div><i class="pe-7s-star"></i></div>	
+
                                 <h3>SOBRANCELHA</h3>
                                 <p>
+						</div>
+				<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
 				<ul>
 					Designe</br>
 					Henna</br>
@@ -440,9 +449,12 @@ Valorização humana</br>
 				</p>
                         </div>
 			<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-	                        <div><i class="pe-7s-star"></i></div>	
+
                                 <h3>MAQUIAGEM</h3>
                                 <p>
+			</div>
+			
+			<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
 				<ul>
 					MakeUp Casual</br>
 					MakeUp Comemorações</br>
@@ -455,9 +467,11 @@ Valorização humana</br>
                     </div>
 		<div class="col-md-4">
 			<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-	                        <div><i class="pe-7s-star"></i></div>	
+
                                 <h3>UNHA</h3>
                                 <p>
+			</div>
+			<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
 				<ul>
 					Mão</br>
 					Pé</br>
@@ -467,9 +481,11 @@ Valorização humana</br>
 				</p>
                         </div>
                         <div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
-	                        <div><i class="pe-7s-star"></i></div>	
+
                                 <h3>ESTÉTICA</h3>
                                 <p>
+						</div>
+				<div class="about_phone wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".5s">
 				<ul>
 					Depilação Axilas</br>
 					Depilação Buço</br>
@@ -537,92 +553,86 @@ Valorização humana</br>
 ============================== -->
     <section class="contact page" id="CONTACT">
         <div class="section_overlay">
-            <div class="container">
-                <div class="col-md-10 col-md-offset-1 wow bounceIn">
-                    <!-- Start Contact Section Title-->
-                    <div class="section_title" style="text-align:center;padding-top:30px !important">
-			<h3>LOCALIZAÇÃO/CONTATO</h3>
+			<!-- Start Contact Section Title-->
+			<div class="section_title" >
+				<h2>LOCALIZAÇÃO / CONTATO</h2>
 			</div>
+			<div class="google-maps">
+				<p style="text-align:center;">
+					<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCVPKwrK-FxWaTUuJdJyJ3cSiwxfZ_SIiQ&q=Salao+Mais+Bonita,Teresina" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</p> 
+			</div>
+	
+			 <div class="container">
+				<div class="col-md-10 col-md-offset-1 wow bounceIn">
 
-<div>
-	<div class="show-only-mobile">
-		<p style="text-align:center;">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.5812980096234!2d-46.52986498536958!3d-23.583472884671963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5d9c8d901537%3A0x866efb293d481b76!2sR.+Manoel+Ferreira+P%C3%ADres%2C+107+-+Jardim+Colorado%2C+S%C3%A3o+Paulo+-+SP%2C+03386-090!5e0!3m2!1sen!2sbr!4v1479429641586" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</p>
-	</div>
-	<div class="mobileHide">
-		<p style="text-align:center;">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.5812980096234!2d-46.52986498536958!3d-23.583472884671963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5d9c8d901537%3A0x866efb293d481b76!2sR.+Manoel+Ferreira+P%C3%ADres%2C+107+-+Jardim+Colorado%2C+S%C3%A3o+Paulo+-+SP%2C+03386-090!5e0!3m2!1sen!2sbr!4v1479429641586" width="800" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</p>			
-	</div>
-	<div style="text-align:center;padding-top:5px !important">
-		<p>
-                	<a class="btn-waze" target="_blank" href="https://waze.to/lr/h6gycv4z85">
-                		<span>Ver no <b>Waze</b></span> 
-                	</a>
-                </p>
-                <p>
-               		<a style="text-decoration:none;background-color:#4285f4;border-radius:2px;color:#fff;display:inline-block;font-weight:bold;min-height:40px;min-width:300px;text-align:center" href="https://goo.gl/app/maps?link=https://www.google.com.br/maps/place/R.%2BManoel%2BFerreira%2BP%25C3%25ADres,%2B107%2B-%2BJardim%2BColorado,%2BS%25C3%25A3o%2BPaulo%2B-%2BSP,%2B03386-090/@-23.5834674,-46.5298646,17z/data%3D!3m1!4b1!4m5!3m4!1s0x94ce5d9c8d901537:0x866efb293d481b76!8m2!3d-23.5834723!4d-46.5276759?hl%3Dpt-BR%26utm_source%3Dapp-invite%26mt%3D8%26pt%3D9008%26utm_medium%3DSIMPLE%26utm_campaign%3Ds2e-ai%26ct%3Ds2e-ai&amp;apn=com.google.android.apps.maps&amp;amv=703000000&amp;isi=585027354&amp;ibi=com.google.Maps&amp;ius=comgooglemapsurl&amp;utm_source=app-invite&amp;mt=8&amp;pt=9008&amp;utm_medium=SIMPLE&amp;utm_campaign=s2e-ai&amp;ct=s2e-ai&amp;invitation_id=493454522602-6273aa21-b841-4749-a33b-c877ae7f6e07" target="_blank"> 
-                		<span style="line-height:40px">Ver no Google MAPS</span> 
-                	</a>
-        	</p>
-	<div>
-</div>				
+					<div style="text-align:center;padding-top:5px !important">
+							<p>
+								<a class="btn-waze" target="_blank" href="https://waze.com/ul/h7p1e4rwtr">
+									<span>Ver no <b>Waze</b></span> 
+								</a>
+							</p>
+							<p>
+								<a style="text-decoration:none;background-color:#4285f4;border-radius:2px;color:#fff;display:inline-block;font-weight:bold;min-height:40px;min-width:300px;text-align:center" href="https://goo.gl/app/maps?link=https://goo.gl/maps/PXrN4Xcr5Zx" target="_blank"> 
+									<span style="line-height:40px">Ver no Google MAPS</span> 
+								</a>
+						</p>				
 
-		<div class="section_title" style="padding-top:5px !important">
-                        <p style="margin-top:5px !important">Rua Manoel Ferreira Pires, 107, São Paulo, SP </br>
-				<a href="https://api.whatsapp.com/send?phone=5511985000092&text=Ol%C3%A1%20WillSalon" target="_blank"><i class="fa fa-whatsapp"></i>(11) 98500-0092 </a></br> 
-				(11) 2809-1414 </br>
-				willsalon@willsalon.com
-			</p>
-                        <div class="social_icons">
-                            <ul>
-                                <li><a target="_blank" href="http://facebook.com/willsalon1"><i class="fa fa-facebook-official" style="font-size:48px;color:#4a90e2;"></i></a>
-                                </li>
-                               <li><a target="_blank" href="http://instagram.com/_u/willsalon/"><i class="fa fa-instagram" style="font-size:48px;color:#fb3958;"></i></a>
-                               </li>
-                            </ul>
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
+						<div class="section_title" style="padding-top:5px !important">
+									<p style="margin-top:5px !important">Av. Homero Castelo Branco, 3072 - Ininga, Teresina - PI</br>
+							<a href="https://api.whatsapp.com/send?phone=558699959320&text=Ol%C3%A1%20Salão Mais Bonita" target="_blank"><i class="fa fa-whatsapp"></i> (86) 9995-9320</a></br> 
+							 (86) 3231-1426</br>
+							</p>
+									<div class="social_icons">
+										<ul>
+											<li><a target="_blank" href="https://www.facebook.com/salaomaisbonita/"><i class="fa fa-facebook-official" style="font-size:48px;color:#4a90e2;"></i></a>
+											</li>
+										   <li><a target="_blank" href="http://instagram.com/_u/salaomaisbonita/"><i class="fa fa-instagram" style="font-size:48px;color:#fb3958;"></i></a>
+										   </li>
+										</ul>
+									</div>
+									
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="contact_form wow bounceIn">
+			<div class="container">
+				<div class="section_title" style="padding:0px !important;text-align:center;font-weight: bold">
+					<h4> Dúvidas ou comentários, envie sua mensagem para nós! </h4>
+				</div>
+			<!-- START ERROR AND SUCCESS MESSAGE -->
+				<div class="form_error text-center">
+					<div class="name_error hide error">Por favor, informe o Nome</div>
+					<div class="email_error hide error">Por favor, informe o Email</div>
+					<div class="email_val_error hide error">Por favor, informe um Email válido</div>
+					<div class="message_error hide error">Por favor, informe a Mensagem</div>
+				</div>
+				<div class="Sucess"></div>
+			<!-- END ERROR AND SUCCESS MESSAGE -->
 
-            <div class="contact_form wow bounceIn">
-                <div class="container">
-
-                <!-- START ERROR AND SUCCESS MESSAGE -->
-                    <div class="form_error text-center">
-                        <div class="name_error hide error">Por favor, informe o Nome</div>
-                        <div class="email_error hide error">Por favor, informe o Email</div>
-                        <div class="email_val_error hide error">Por favor, informe um Email válido</div>
-                        <div class="message_error hide error">Por favor, informe a Mensagem</div>
-                    </div>
-                    <div class="Sucess"></div>
-                <!-- END ERROR AND SUCCESS MESSAGE -->
-
-                <!-- FORM -->    
-                    <form role="form" method="post" action="envia.php">
-			<input id="emailD" type="hidden" name="emaildestinatario" value="willsalon@willsalon.com"/>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <input type="text" name="nomeremetente" class="form-control" id="name" placeholder="Nome" required>
-                                <input type="email" name="emailremetente" class="form-control" id="email" placeholder="Email" required>
-                                <input type="text" name="assunto" class="form-control" id="subject" placeholder="Assunto" required>
-                            </div>
+			<!-- FORM -->    
+				<form role="form" method="post" action="envia.php">
+		<input id="emailD" type="hidden" name="emaildestinatario" value="maisbonita@ig.com.br"/>
+					<div class="row">
+						<div class="col-md-4">
+							<input type="text" name="nomeremetente" class="form-control" id="name" placeholder="Nome" required>
+							<input type="email" name="emailremetente" class="form-control" id="email" placeholder="Email" required>
+							<input type="text" name="assunto" class="form-control" id="subject" placeholder="Assunto" required>
+						</div>
 
 
-                            <div class="col-md-8">
-                                <textarea class="form-control" name="mensagem" id="message" rows="25" cols="10" placeholder="  Mensagem..." required></textarea>
-                                <button type="submit" name="Submit" class="btn btn-default submit-btn form_submit">Enviar!</button>
-                            </div>
-                        </div>
-                    </form>
-                    <!-- END FORM --> 
-                </div>
-            </div>
-
-        </div>
+						<div class="col-md-8">
+							<textarea class="form-control" name="mensagem" id="message" rows="25" cols="10" placeholder="Mensagem..." required></textarea>
+							<button type="submit" name="Submit" class="btn btn-default submit-btn form_submit">Enviar!</button>
+						</div>
+					</div>
+				</form>
+				<!-- END FORM --> 
+			</div>
+		</div>
+		
     </section>
     <!-- END CONTACT -->
 
@@ -635,17 +645,17 @@ Valorização humana</br>
         <h2></h2>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <div class="copy_right_text">
-                    <!-- COPYRIGHT TEXT -->
-                        <p>Copyright &copy; at 2016 <a href="">WILL SALON</a> <span>By </span></p>
-                        <p><a target="blank" href="https://br.linkedin.com/in/perfilrafaelfernandes">Rafael Fernandes</a></p>
-                    </div>
-                </div>
-
-                <div class="col-md-6">
+				<div class="col-md-12">
                     <div class="scroll_top">
                         <a href="#HOME"><i class="fa fa-angle-up"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="copy_right_text">
+                    <!-- COPYRIGHT TEXT -->
+                        <p>Copyright &copy; - Todos os direitos reservados. <span></p>
+						<p>By </span>  <a target="blank" href="https://br.linkedin.com/in/perfilrafaelfernandes">Rafael Fernandes</a> e </span>  <a target="blank" href="https://www.linkedin.com/in/perfilpedrotome/">Pedro Tomé</a></p>
+                       
                     </div>
                 </div>
             </div>
